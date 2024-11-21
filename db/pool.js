4f66c82903de6c174cd87777fa3ff42ef42ee9ef
Ahmed <ml967799@gmail.com>
2024-11-21 20:17:34 +0100

    connecting to the data base

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,6 +8,11 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
 })
+*/
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+});
 
 
 module.exports = pool;
