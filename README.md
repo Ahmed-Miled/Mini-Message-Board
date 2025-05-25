b3e110d137fa4ca75d247f7d08d6c75b2c931a85
Ahmed-Miled <ml967799@gmail.com>
2025-05-25 17:50:09 +0100

    updating readme && pool

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 **tech used**  
 Routes, controllers and views
+
+**in order to use**  
+
+npm run seed <br>
+npm run dev <br>
--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-/*
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,11 +8,11 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
 })
-*/
+/*
 // Use DATABASE_URL for cloud deployment, fallback to individual variables for local development
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // Use the database URL directly
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // Only enable SSL for cloud-based DB
 });
-
+*/
 module.exports = pool;
